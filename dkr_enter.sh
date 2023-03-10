b51d6b07d01adcd9c9f218fafdf3ffd7352ada56
rodion <jobbahot@gmail.com>
2023-03-10 23:56:11 +0100

    doker helper script update

--- dkr_enter.sh
+++ dkr_enter.sh
@@ -1,1 +1,24 @@
-docker run -it -v $(pwd):/chalkboard --rm --entrypoint=/bin/sh ${1}
+if [[ "$2" == "-" ]]
+then
+    ENTRY=""
+elif [[ "$2" == "." ]]
+then
+    ENTRY="--entrypoint=/bin/sh"
+else
+    ENTRY="--entrypoint=$2"
+fi
+
+if [[ "$3" == "-" ]]
+then
+    PORT=""
+elif [[ "$3" == "" ]]
+then
+    PORT=""
+else
+    PORT="-p $3:$3"
+fi
+
+echo "Starting $ENTRY in image $1 ports: $PORT"
+
+echo docker run -it $PORT -v $(pwd):/chalkboard --rm $ENTRY $1 $4 $5 $6 $7 $8 $9
+docker run -it $PORT -v $(pwd):/chalkboard --rm $ENTRY $1 $4 $5 $6 $7 $8 $9
