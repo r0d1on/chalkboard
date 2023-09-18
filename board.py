258aab19a4d2a5b15c9e579fc3dfd1ad4c26299e
rodion <jobbahot@gmail.com>
2023-09-18 22:19:56 +0200

    dropping backend-based persistence support and poll-syncing

--- board.py
+++ board.py
@@ -7,10 +7,6 @@ import gzip
 from io import BytesIO, StringIO
 
 
-CONFIG = {
-    "dry" : False
-}
-
 app = flask.Flask(__name__
                 ,static_url_path = ''
                 ,static_folder   = 'static'
@@ -20,75 +16,6 @@ app = flask.Flask(__name__
 @app.route('/', methods=['GET'])
 def http_root():
     return flask.redirect(flask.url_for('static',filename='index.html#about').replace('%23','#'))
-
-
-def sync_message(loc, msg):
-    for in_commit, in_strokes in msg['strokes'].items():
-        loc['strokes'][in_commit] = loc['strokes'].get(in_commit, {})
-        for in_idx, in_stroke in in_strokes.items():
-            own_stroke = loc['strokes'][in_commit].get(in_idx, None)
-            if (own_stroke is None)or(in_stroke['version'] > own_stroke['version']):
-                loc['strokes'][in_commit][in_idx] = in_stroke
-
-    return
-
-
-def update_modules(loc, msg):
-    def upd(t, s):
-        c = False
-        
-        if s is None:
-            return False
-        
-        if (t is None)or(type(t)!=type(s)):
-            return True
-        
-        if (type(s)==dict):
-            for k in s:
-                if s[k] is None:
-                    pass
-                elif (t.get(k, None) is None)or(type(t[k])!=type(s[k])):
-                    t[k] = s[k]
-                    c = True
-                else:
-                    if (isinstance(s[k], (list, dict))):
-                        c = c or upd(t[k], s[k])
-                    elif t[k] != s[k]:
-                        t[k] = s[k]
-                        c = True
-        elif (type(s)==list):
-            for i in range(len(s)):
-                if (i>=len(t)) or (t[i] is None)or(type(t[i])!=type(s[i])):
-                    if i>=len(t):
-                        t.append(s[i])
-                    else:
-                        t[i] = s[i]
-                    c = True
-                else:
-                    if (isinstance(s[i], (list, dict))):
-                        c = c or upd(t[i], s[i])
-                    elif t[i] != s[i]:
-                        t[i] = s[i]
-                        c = True
-        elif type(s)==None:
-            c = False
-        else:
-            c = t!=s
-        return c
-        
-    loc["modules"] = loc.get("modules", {})
-    if (len(loc["modules"])==0):
-        print(' - empty local modules')
-
-    print()
-    print('received modules:', msg.get("modules",{}))
-    
-    changed = upd(loc["modules"], msg.get("modules",{}))
-    
-    print()
-    print('updated modules:', changed, msg.get("modules",{}))
-    
-    return changed
 
 
 def _compress(s):
@@ -131,7 +58,7 @@ def http_record_load():
 def http_record_save():
     content_type = flask.request.headers['Content-Type'].split(";")[0]
     content = None
-    
+
     if (content_type == 'application/json'):
         msg = flask.request.get_json(force=True)
         content = _compress(json.dumps(msg))
@@ -153,118 +80,6 @@ def http_record_save():
     
     return json.dumps([])
 
-
-@app.route("/sync", methods=['POST'])
-def http_sync():
-    msg = flask.request.get_json(force=True)
-
-    print()
-    print('=='*30)
-    print("<= brd=", msg['name'], ' ver=', msg['version'], ' |msg|=', len(msg['strokes']))
-
-    if (msg["version"] < 0) and (CONFIG['dry']):
-        raise Exception("backend is not available")
-
-    bname = msg['name'].split('!', 1)
-    if len(bname)==1:
-        bpass = ""
-    else:
-        bpass = bname[1]
-    bname = bname[0]
-
-    bfile = "boards/brd_" + (re.sub("\\\|/|%|\.", "_", bname)) + ".json"
-
-    changed = False
-
-    if (os.path.isfile(bfile)):
-        try:
-            with open(bfile, 'rt') as f:
-                loc = json.loads(f.read())
-        except Exception as ex:
-            print("ex: ", ex)
-            return json.dumps({
-                "resync" : 1
-            })
-    else:
-        print("new board:", bfile)
-        loc = {
-             "version" : 0
-            ,"strokes" : {}
-            ,"view_rect" : None
-            ,"slides" : []
-            ,"p" : bpass
-        }
-        changed = True
-        
-    if loc.get("PERSISTENCE_VERSION", None) is None:
-        loc["PERSISTENCE_VERSION"] = 2
-
-    if loc["PERSISTENCE_VERSION"] < 4:
-        print('persistence update')
-        loc["modules"] = {
-            "slider" : {
-                 "view_rect" : loc.get("view_rect", None)
-                ,"slides" : loc.get("slides", None)
-            }
-        }
-        if "view_rect" in loc:
-            del loc["view_rect"]
-        if "slides" in loc:
-            del loc["slides"]
-
-    auth = ((bpass=="") or (loc['p']==bpass))
-
-    # process the request (ingest remote updates)
-    
-    changed = (auth and update_modules(loc, msg)) or changed;
-
-    if (loc["version"] < msg["version"]) and (auth): # new version incoming
-        loc["version"] = msg["version"]
-        loc["p"] = bpass
-        changed = True
-
-    if (len(msg["strokes"]) > 0) and (auth): # have some strokes incoming
-        
-        if (loc["PERSISTENCE_VERSION"] < msg["PERSISTENCE_VERSION"]): # format update
-            msg["refresh"] = 1
-        
-        if (msg["refresh"] == 1): # full reset requested
-            loc["version"] = 0
-            loc["strokes"] = {} 
-        
-        sync_message(loc, msg) # sync in the data
-        changed = True
-
-    if changed:
-        loc["PERSISTENCE_VERSION"] = max(loc["PERSISTENCE_VERSION"], msg["PERSISTENCE_VERSION"])
-        with open(bfile, 'wt') as f:
-            f.write(json.dumps(loc))
-        print("updated board:", bfile)
-
-    # prepare response with local updates
-    upd = {}
-    for loc_commit, loc_strokes in loc['strokes'].items():
-        for loc_idx, loc_stroke in loc_strokes.items():
-            if (loc_stroke.get('version', None) is None):
-                print('loc: ', loc_stroke)
-            if (msg.get('version', None) is None):
-                print('msg: ', msg)
-                
-            if (loc_stroke['version'] > msg['version']):
-                upd[loc_commit] = upd.get(loc_commit, {})
-                upd[loc_commit][loc_idx] = loc_stroke
-
-    print("=> brd=", msg['name'], ' |upd|=', len(upd))
-
-
-    return json.dumps({
-          "strokes" : upd
-         ,"received_version" : msg["version"]
-         ,"modules" : loc.get("modules", {})
-         ,"PERSISTENCE_VERSION" : loc["PERSISTENCE_VERSION"]
-    })
-
-
 if __name__ == '__main__':
     debug = False
     port = 5000
@@ -277,12 +92,6 @@ if __name__ == '__main__':
         
         if '--bind' in sys.argv:
             bind_ip = sys.argv[sys.argv.index('--bind') + 1]
-
-        if '--dry' in sys.argv:
-            CONFIG['dry'] = sys.argv[sys.argv.index('--dry') + 1]
-        else:
-            CONFIG['dry'] = False
-        print('dry mode:', CONFIG['dry'])
 
     if debug:
         app.run(host=bind_ip, port=port, threaded=False, debug=True)
--- static/actions/SAVE.js
+++ static/actions/SAVE.js
@@ -17,17 +17,11 @@ let SAVE = {
     ,icon : [null,[8,7],[8,11],[8,13],[7,17],[8,19],[7,23],[8,25],[7,28],[8,30],[7,34],[8,36],[7,40],[8,42],[7,45],[8,47],[7,51],[8,53],[10,54],[14,53],[16,54],[19,53],[21,54],[25,53],[27,54],[31,53],[33,54],[36,53],[38,54],[42,53],[44,54],[48,53],[50,54],[53,53],[53,51],[53,48],[53,45],[54,42],[53,39],[53,36],[53,34],[53,30],[53,26],[53,22],[53,18],null,[44,8],[40,8],[36,7],[33,8],[30,8],[27,8],[25,8],[22,8],[19,7],[16,8],[13,8],[10,8],[8,7],null,[53,18],[44,8],[53,18],null,[15,10],[15,21],[37,21],[37,9],[37,21],[15,21],[15,10],null,[14,51],[14,31],[14,51],null,[14,31],[42,30],[42,51],[42,30],[14,31],null,[20,36],[35,36],null,[21,45],[35,45],null,[19,12],[33,12],null,[17,17],[33,16]]
     ,icon_save : [null,[16,12],[16,15],[16,19],[16,24],null,[16,34],[16,36],[16,41],[16,45],[18,47],[20,46],[25,46],[29,46],[33,47],[37,47],[41,47],[46,47],[49,46],[49,43],[50,40],[50,38],[49,36],[49,32],[49,29],[49,26],[49,23],[49,20],null,[43,13],[40,13],[37,12],[35,12],[31,13],[27,13],[24,12],[22,12],[16,12],null,[49,20],[43,13],[49,20],null,[22,12],[21,20],[37,20],[37,12],[37,20],[21,20],[22,12],null,[35,15],[24,15],[35,15],null,[25,35],[30,30],[25,25],null,[4,30],[30,30],[25,35],null,[25,25],[30,30],[4,30]]
     ,icon_load : [null,[19,15],[19,18],[19,22],[19,26],null,[19,36],[18,39],[18,43],[18,48],[20,50],[23,49],[27,49],[31,49],[36,49],[40,49],[44,49],[48,49],[52,49],[52,46],[52,43],[52,41],[52,38],[52,35],[52,32],[52,29],[52,26],[52,23],null,[45,15],[43,15],[40,15],[37,15],[33,16],[29,15],[27,15],[25,15],[19,15],null,[52,23],[45,15],[52,23],null,[25,15],[24,23],[39,23],[40,15],[39,23],[24,23],[25,15],null,[37,18],[26,18],[37,18],null,[10,27],[5,32],[10,37],null,[31,32],[5,32],[10,27],null,[10,37],[5,32],[31,32]]
-    ,icon_sync : [null,[45,25],[50,29],[54,25],null,[50,24],[50,29],null,[13,33],[8,31],[5,35],null,[9,36],[8,31],null,[45,25],[50,29],[54,25],null,[13,33],[8,31],[5,35],null,[23,50],[20,49],[17,47],[14,45],[12,42],[10,39],[9,36],null,[8,23],[10,20],[11,18],[14,15],[16,13],[19,11],[23,10],[26,9],[30,8],[33,9],[36,9],[40,11],[42,13],[45,15],[48,18],[49,21],[50,24],null,[51,37],[49,40],[48,43],[45,46],[43,47],[40,49],[36,50],[33,51],[29,51],[26,51],[23,50]]
     ,icon_download_group : [null,[20,51],[40,51],null,[52,39],[52,20],null,[8,20],[8,39],null,[40,8],[20,8],null,[42,30],[30,42],[18,30],null,[30,18],[30,42],[42,30],null,[18,30],[30,42],[30,18],null,[11,16],[16,11],null,[44,11],[50,16],null,[10,43],[16,49],null,[49,43],[44,48]]
     ,icon_download : [null,[9,50],[9,10],[51,10],null,[9,50],[30,50],null,[51,30],[51,10],[9,10],null,[51,10],[51,30],null,[9,10],[9,50],[30,50],null,[40,35],[48,35],null,[53,40],[53,48],null,[35,48],[35,40],null,[48,53],[40,53],null,[37,52],[37,52],null,[37,36],[37,36],null,[51,37],[51,37],null,[51,52],[51,52],null,[39,44],[44,49],[49,44],null,[44,39],[44,49],[39,44],null,[49,44],[44,49],[44,39],null,[17,19],[25,19],null,[33,19],[43,19],null,[17,27],[43,27],null,[27,34],[27,43],null,[18,34],[18,43]]
     ,icon_download_png_board : [null,[8,49],[8,9],[50,9],null,[8,49],[29,49],null,[50,29],[50,9],[8,9],null,[50,9],[50,29],null,[8,9],[8,49],[29,49],null,[47,52],[39,52],null,[34,47],[34,39],null,[52,39],[52,47],null,[39,34],[47,34],null,[50,35],[50,35],null,[50,51],[50,51],null,[36,50],[36,50],null,[36,35],[36,35],null,[38,43],[43,48],[48,43],null,[43,38],[43,48],[38,43],null,[48,43],[43,48],[43,38],null,[14,21],[14,21],[14,21],[14,21],[14,21],[14,21],[14,21],[14,24],[14,24],null,[18,15],[18,15],[18,15],[18,15],[18,15],[18,15],[18,15],[21,17],[21,17],[21,17],[21,17],[21,17],[21,17],[21,21],[21,21],[21,21],[18,21],[18,21],[14,21],[14,21],[14,21],null,[43,16],[43,16],[43,16],[43,16],[43,16],[43,16],[39,17],[39,17],[39,17],[37,19],[37,19],[37,19],[37,19],[37,19],[36,22],[36,22],[36,22],[36,22],[36,22],[36,22],[39,25],[39,25],[39,25],[39,25],[39,25],[43,25],[43,25],[43,25],[43,25],[43,25],[43,25],[43,25],[45,22],[45,22],[45,22],[45,22],[45,22],[42,21],[42,21],null,[14,16],[14,16],[14,16],[14,16],[14,16],[14,16],[14,21],null,[14,16],[14,16],[14,16],[14,16],[14,16],[18,15],null,[25,16],[25,16],[25,16],[25,16],[25,16],[25,20],[25,20],[25,20],[25,20],[25,20],[25,24],[25,24],[25,24],null,[25,16],[25,16],[25,16],[25,16],[25,16],[25,16],[25,20],[25,20],[25,20],null,[32,21],[32,21],[32,21],[32,21],[32,21],[32,21],null,[32,24],[32,24],[32,24],[32,24],[32,24],[32,24],[32,24],[32,21],[32,16],[32,16],[32,16],[32,16],null,[25,20],[28,22],[28,22],[28,22],[28,22],[32,24]]
     ,icon_download_png_view : [null,[47,51],[39,51],null,[34,46],[34,38],null,[52,38],[52,46],null,[39,33],[47,33],null,[50,34],[50,34],null,[50,50],[50,50],null,[36,49],[36,49],null,[36,34],[36,34],null,[38,42],[43,47],[48,42],null,[43,37],[43,47],[38,42],null,[48,42],[43,47],[43,37],null,[14,20],[14,20],[14,20],[14,20],[14,20],[14,20],[14,20],[14,23],[14,23],null,[18,14],[18,14],[18,14],[18,14],[18,14],[18,14],[18,14],[21,16],[21,16],[21,16],[21,16],[21,16],[21,16],[21,20],[21,20],[21,20],[18,20],[18,20],[14,20],[14,20],[14,20],null,[43,15],[43,15],[43,15],[43,15],[43,15],[43,15],[39,16],[39,16],[39,16],[37,18],[37,18],[37,18],[37,18],[37,18],[36,21],[36,21],[36,21],[36,21],[36,21],[36,21],[39,24],[39,24],[39,24],[39,24],[39,24],[43,24],[43,24],[43,24],[43,24],[43,24],[43,24],[43,24],[45,21],[45,21],[45,21],[45,21],[45,21],[42,20],[42,20],null,[14,15],[14,15],[14,15],[14,15],[14,15],[14,15],[14,20],null,[14,15],[14,15],[14,15],[14,15],[14,15],[18,14],null,[25,15],[25,15],[25,15],[25,15],[25,15],[25,19],[25,19],[25,19],[25,19],[25,19],[25,23],[25,23],[25,23],null,[25,15],[25,15],[25,15],[25,15],[25,15],[25,15],[25,19],[25,19],[25,19],null,[32,20],[32,20],[32,20],[32,20],[32,20],[32,20],null,[32,23],[32,23],[32,23],[32,23],[32,23],[32,23],[32,23],[32,20],[32,15],[32,15],[32,15],[32,15],null,[25,19],[28,21],[28,21],[28,21],[28,21],[32,23],null,[8,8],[13,8],[8,8],null,[44,8],[50,8],[44,8],null,[50,14],[50,8],[50,14],null,[8,14],[8,8],[8,14],null,[14,48],[8,48],[14,48],null,[8,48],[8,42],[8,48],null,[24,8],[24,8],[24,8],null,[34,8],[34,8],[34,8],null,[50,24],[50,24],[50,24],null,[8,23],[8,23],[8,23],null,[8,33],[8,33],[8,33],null,[23,48],[23,48],[23,48]]
     ,icon_upload : [null,[40,9],[20,9],null,[8,21],[8,40],null,[52,40],[52,21],null,[20,52],[40,52],null,[18,30],[30,18],[42,30],null,[30,42],[30,18],[18,30],null,[42,30],[30,18],[30,42],null,[49,44],[44,49],null,[16,49],[10,44],null,[50,17],[44,11],null,[11,17],[16,12]]
-
-    ,autosync : false
-    ,sent_version : null
-    ,is_syncing : false
-    ,canvas_sync : null
 
     ,_strokes_to_save : function(from_version) {
         let out_strokes = {};
@@ -361,169 +355,6 @@ let SAVE = {
         SAVE.MENU_main.hide('save_group');
     }
 
-    ,sync_message : function(msg, is_sync) {
-        let max_commit = '';
-        let max_stroke_id = '';
-        let loaded = false;
-
-        if ((msg.PERSISTENCE_VERSION===undefined)||(msg.PERSISTENCE_VERSION===null))
-            msg.PERSISTENCE_VERSION = 2;
-
-        msg = SAVE._unpersist_message(msg);
-        UI.on_unpersist(msg, is_sync);
-
-        if (!is_sync)
-            SAVE._fix_collisions(msg.strokes);
-
-        for(let in_commit in msg.strokes) {
-            let in_strokes = msg.strokes[in_commit];
-            BOARD.strokes[in_commit] = BOARD.strokes[in_commit] || {};
-
-            for(let in_idx in in_strokes) {
-                let in_stroke = in_strokes[in_idx];
-                let own_stroke = BOARD.strokes[in_commit][in_idx];
-
-                if ((own_stroke===undefined)||(in_stroke['version'] > own_stroke['version'])) {
-                    BOARD.register(in_stroke);
-                }
-
-                BOARD.version = (BOARD.version > in_stroke.version) ? BOARD.version : in_stroke.version;
-                max_commit = (max_commit > in_stroke.commit_id) ? max_commit : in_stroke.commit_id;
-                max_stroke_id = (max_stroke_id > in_stroke.stroke_id) ? max_stroke_id : in_stroke.stroke_id;
-            }
-        }
-
-        if (!is_sync)
-            SAVE._update_ids();
-
-        if (sizeof(msg.strokes) > 0) {
-            BOARD.drop_redo();
-
-            max_commit = max_commit.split('-')[0];
-            max_commit = (BOARD.commit_id.split('-')[0] > max_commit)?BOARD.commit_id.split('-')[0]:max_commit;
-
-            max_stroke_id = max_stroke_id.split('-')[0];
-            max_stroke_id = (BOARD.stroke_id.split('-')[0] > max_stroke_id)?BOARD.stroke_id.split('-')[0]:max_stroke_id;
-
-            BOARD.commit_id = BOARD.id_next(max_commit);
-            BOARD.stroke_id = BOARD.id_next(max_stroke_id);
-
-            UI.redraw();
-
-            loaded = true;
-        }
-
-        return loaded;
-    }
-
-    ,_consume_message : function(str_json, is_sync) {
-        let message_in = JSON.parse(str_json);
-        if ((message_in.resync)||(BOARD.locked)) {
-            UI.log(-1, 'will resync:', message_in);
-            return false;
-        } else {
-            SAVE.sent_version = message_in.received_version;
-            let consumed = SAVE.sync_message(message_in, is_sync);
-            return consumed;
-        }
-    }
-
-    ,sync_begin : function() {
-        SAVE.is_syncing = true;
-        UI.set_busy('SAVER', true);
-    }
-
-    ,sync_end : function() {
-        SAVE.is_syncing = false;
-        UI.set_busy('SAVER', false);
-    }
-
-    ,sync : function(full) {
-        if (SAVE.is_syncing) {
-            UI.log(0, 'skipping sync() - already syncing');
-            return;
-        }
-
-        if (BOARD.locked) {
-            UI.log(0, 'skipping sync() - board is locked');
-            return;
-        }
-
-        if (full)
-            SAVE.sent_version = null;
-
-        let from_version = (SAVE.sent_version == null)? 0 : SAVE.sent_version + 1;
-
-        let out_strokes = SAVE._strokes_to_save(from_version);
-
-        let json = {
-            name : BOARD.board_name
-
-            ,version : BOARD.version
-            ,strokes : out_strokes
-            ,modules : {}
-
-            ,PERSISTENCE_VERSION : SAVE.PERSISTENCE_VERSION
-            ,refresh : (SAVE.sent_version == null) ? 1 : 0
-        };
-        UI.on_persist(json, true);
-
-        let message_out = deepcopy(json);
-
-        //console.log("sending: ", message_out.version, "L=", message_out.strokes.length, message_out);
-
-        SAVE.sync_begin();
-
-        UI.IO.request('/sync', message_out, {})
-            .then(({xhr})=>{
-                SAVE._consume_message(xhr.responseText, true);
-                if (full)
-                    UI.toast('backend.saving', 'board saved on the backend', 2000);
-            })
-            .catch(({xhr, error, message})=>{
-                UI.log(-1, 'could not send the data:', error, xhr);
-                UI.log(1, 'message:', message);
-                UI.toast('backend.saving', 'connection to backend is broken, could not sync the board', 2000);
-                if (SAVE.autosync) {
-                    SAVE.sync_switch();
-                }
-            })
-            .finally(()=>{
-                SAVE.sync_end();
-            });
-
-        //console.log("=> |msg|:", sizeof(message_out.strokes), " ver:", message_out.version);
-    }
-
-    ,sync_switch : function(e, id, long) {
-        function _sync() {
-            if (SAVE.is_syncing) {
-                UI.log(0, 'skipping sync: already syncing');
-            } else {
-                SAVE.sync();
-            }
-            if (SAVE.autosync) {
-                setTimeout(_sync, 1000);
-            }
-        }
-
-        if (long) {
-            SAVE.sync(true);
-            return;
-        }
-
-        if (SAVE.autosync) {
-            SAVE.autosync = false;
-        } else {
-            SAVE.autosync = true;
-            _sync();
-        }
-
-        SAVE.canvas_sync.width = SAVE.canvas_sync.width+1-1;
-        let ctx = SAVE.canvas_sync.getContext('2d');
-        UI.draw_glyph(SAVE.icon_sync, ctx, undefined, (SAVE.autosync?undefined:'#555'));
-    }
-
     ,on_file : function(file) {
         function load_json_data(json_data) {
             UI.log(0, 'loaded board file:', file.name);
@@ -570,44 +401,9 @@ let SAVE = {
         ctx = MENU_main.add('save_group', 'upload', SAVE.upload, 'canvas', 'upload board / image')[1].getContext('2d');
         UI.draw_glyph(SAVE.icon_upload, ctx);
 
-        //SAVE.sync_switch();
-
         UI.addEventListener('on_file', SAVE.on_file);
 
         document.getElementById('file-input').addEventListener('change', SAVE.handleFiles);
-
-        let message_out = {
-            name : BOARD.board_name
-            ,version : -1
-            ,strokes : {}
-            ,PERSISTENCE_VERSION : SAVE.PERSISTENCE_VERSION
-        };
-
-        setTimeout(()=>{
-            let loaded = false;
-            SAVE.sync_begin();
-            UI.IO.request('/sync', message_out, {})
-                .then(({xhr})=>{
-                    UI.log(0, 'backend available: ', xhr);
-                    SAVE.canvas_sync = MENU_main.add('save_group', 'sync', SAVE.sync_switch, 'canvas', 'auto-sync to server')[1];
-                    let ctx = SAVE.canvas_sync.getContext('2d');
-                    UI.draw_glyph(SAVE.icon_sync, ctx, undefined, '#555');
-
-                    loaded = SAVE._consume_message(xhr.responseText, false);
-                    if (loaded)
-                        UI.toast('backend.loading', 'loaded from backend', 2000);
-                })
-                .catch(({xhr, error})=>{
-                    UI.log(-1, 'backend unavailable: ', error, xhr);
-                    UI.toast('backend.loading', 'backend is not available : ' + error, 2000);
-                })
-                .finally(()=>{
-                    SAVE.sync_end();
-                    if (!loaded)
-                        SAVE.load();
-                });
-        }, 100);
-
     }
 
 };
